f1c82d423ed41c03f3e28c54c9dce0d5aeb91dcf
komaba461 <xchen279@jh.edu>
2024-12-18 00:10:08 -0500

    update

--- thumbs.sh
+++ thumbs.sh
@@ -1,2 +1,3 @@
+for file in ./images/fulls/*.heic; do magick "$file" "${file%.heic}.jpg" && rm "$file"; done
+for file in ./images/fulls/*.HEIC; do magick "$file" "${file%.HEIC}.jpg" && rm "$file"; done
 mogrify -resize 25% -quality 100 -path ./images/thumbs ./images/fulls/*.jpg
-mogrify -resize 25% -quality 100 -path ./images/thumbs ./images/fulls/*.heic
